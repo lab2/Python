da9faf36b9718cb892cd1833360dec4a7869bf6a
mocu <curzio.moriggia@sunrise.ch>
2023-12-16 12:40:53 +0100

    Simple REST API

---
+++ rest.py
@@ -0,0 +1,16 @@
+from flask import Flask, json
+
+books = [{"id": 1, "name": "Java in action"}, {"id": 2, "name": "Github in a nutshell"}]
+
+api = Flask(__name__)
+
+@api.route('/books', methods=['GET'])
+def get_books():
+    return json.dumps(books)
+
+@api.route('/first', methods=['GET'])
+def get_first():
+    return json.dumps(books[0])
+
+if __name__ == '__main__':
+    api.run()
