7ceab3b43a7213e04f2e0d1a3954844e6ee13242
mocu <curzio.moriggia@sunrise.ch>
2023-12-18 08:29:36 +0100

    Enhancements

--- tests/test_rest.py
+++ tests/test_rest.py
@@ -1,26 +1,32 @@
 # REST tests
 # Refer to source simple_rest_api.py
 
-import requests
 import os
+
 import pytest
+import requests
 
 IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
 
 urls = ["http://localhost:5000/first", "http://localhost:5000/books"]
+
+
 def make_request(base_url):
     response = requests.get(f'{base_url}')
     return response
+
 
 @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github actions")
 def test_rest_with_sucess():
     assert '200' in str(make_request(urls[0]))
     assert '200' in str(make_request(urls[1]))
 
+
 @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 def test_rest_with_fail():
     assert "Java in action" in str(make_request(urls[1]).content)
     assert "Github in a nutshell" in str(make_request(urls[0]).content)
+
 
 @pytest.mark.skipif(IN_GITHUB_ACTIONS, reason="Test doesn't work in Github Actions.")
 def test_rest_with_content():
